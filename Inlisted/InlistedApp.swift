26f4e8c60ee2572ddfb31557917477e1135ea8ed
Brandon Hollins <brandonhollins4@gmail.com>
2023-08-03 00:20:25 -0500

    Initial Commit

---
+++ Inlisted/InlistedApp.swift
@@ -0,0 +1,17 @@
+//
+//  InlistedApp.swift
+//  Inlisted
+//
+//  Created by Brandon D Hollins on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct InlistedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
